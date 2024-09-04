312597728681c99ee67304c409b2a8955c4ca08f
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-09-04 21:38:00 +0900

    TEST : 대문자를 포함하지 않고 나머지 조건을 충족하는 경우

--- src/test/java/com/example/practiceTdd/chap02/PasswordStrengthMeter.java
+++ src/test/java/com/example/practiceTdd/chap02/PasswordStrengthMeter.java
@@ -15,12 +15,26 @@ public class PasswordStrengthMeter {
             return PasswordStrength.NORMAL;
         }
 
+        boolean containsUpper = meetsContainingUppercaseCriteria(s);
+        if(!containsUpper){
+            return PasswordStrength.NORMAL;
+        }
+
         return PasswordStrength.STRONG;
     }
 
     private boolean meetsContainingNumberCriteria(String s){
         for(char c : s.toCharArray()){
             if(c >= '0' && c<= '9'){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean meetsContainingUppercaseCriteria(String s){
+        for(char c : s.toCharArray()){
+            if(Character.isUpperCase(c)){
                 return true;
             }
         }
