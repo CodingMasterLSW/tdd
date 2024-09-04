60b63fcae136b3871907076069e6627086e5e0f3
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-09-04 21:55:22 +0900

    TEST : PasswordStrengthMeter 테스트코드 리팩토링

--- src/test/java/com/example/practiceTdd/chap02/PasswordStrengthMeter.java
+++ src/test/java/com/example/practiceTdd/chap02/PasswordStrengthMeter.java
@@ -1,55 +1,49 @@
 package com.example.practiceTdd.chap02;
 
 public class PasswordStrengthMeter {
-    public PasswordStrength meter(String s){
-        if(s == null || s.isEmpty()){
+
+    public PasswordStrength meter(String s) {
+        if (s == null || s.isEmpty()) {
             return PasswordStrength.INVALID;
         }
 
+        int metCounts = 0;
         boolean lengthEnough = s.length() >= 8;
+        if (lengthEnough) {
+            metCounts++;
+        }
         boolean containsNum = meetsContainingNumberCriteria(s);
+        if(containsNum){
+            metCounts++;
+        }
         boolean containsUpp = meetsContainingUppercaseCriteria(s);
+        if(containsUpp){
+            metCounts++;
+        }
 
-        if(lengthEnough && !containsNum && !containsUpp){
+        if(metCounts == 1){
             return PasswordStrength.WEAK;
         }
 
-        if(!lengthEnough && containsNum && !containsUpp){
-            return PasswordStrength.WEAK;
-        }
-
-        if(!lengthEnough && !containsNum && containsUpp){
-            return PasswordStrength.WEAK;
-        }
-
-
-        if(s.length()<8){
-            return PasswordStrength.NORMAL;
-        }
-
-        if(!containsNum){
-            return PasswordStrength.NORMAL;
-        }
-
-        if(!containsUpp){
+        if(metCounts == 2){
             return PasswordStrength.NORMAL;
         }
 
         return PasswordStrength.STRONG;
     }
 
-    private boolean meetsContainingNumberCriteria(String s){
-        for(char c : s.toCharArray()){
-            if(c >= '0' && c<= '9'){
+    private boolean meetsContainingNumberCriteria(String s) {
+        for (char c : s.toCharArray()) {
+            if (c >= '0' && c <= '9') {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean meetsContainingUppercaseCriteria(String s){
-        for(char c : s.toCharArray()){
-            if(Character.isUpperCase(c)){
+    private boolean meetsContainingUppercaseCriteria(String s) {
+        for (char c : s.toCharArray()) {
+            if (Character.isUpperCase(c)) {
                 return true;
             }
         }
