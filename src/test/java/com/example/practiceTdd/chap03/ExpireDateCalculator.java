0a38bc1dea0746825b9eabf8344bd8d4181b1ed7
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-09-06 00:07:23 +0900

    TEST : 상수값을 리턴해 테스트 오류 해결

---
+++ src/test/java/com/example/practiceTdd/chap03/ExpireDateCalculator.java
@@ -0,0 +1,10 @@
+package com.example.practiceTdd.chap03;
+
+import java.time.LocalDate;
+
+public class ExpireDateCalculator {
+
+    public LocalDate calculatorExpiryDate(LocalDate billingDate, int payAmount) {
+        return LocalDate.of(2019,4,1);
+    }
+}
