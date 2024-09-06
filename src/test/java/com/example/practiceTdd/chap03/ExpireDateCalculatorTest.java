9a27a650f738f82735adedfb26f0457b92cddc8d
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-09-06 22:05:13 +0900

    TEST : 2월 예외상황 테스트 코드 작성

--- src/test/java/com/example/practiceTdd/chap03/ExpireDateCalculatorTest.java
+++ src/test/java/com/example/practiceTdd/chap03/ExpireDateCalculatorTest.java
@@ -19,6 +19,24 @@ public class ExpireDateCalculatorTest {
 
     }
 
+    @Test
+    void 납부일과_한달_뒤_일자가_같지_않음(){
+        assertExpireDate(
+                LocalDate.of(2019,1,31), 10_000,
+                LocalDate.of(2019,2,28));
+
+        assertExpireDate(
+                LocalDate.of(2019,5,31), 10_000,
+                LocalDate.of(2019,6,30)
+        );
+
+        assertExpireDate(
+                LocalDate.of(2020,1,31), 10_000,
+                LocalDate.of(2020,2,29)
+        );
+
+    }
+
     private void assertExpireDate(LocalDate billingDate, int payAmount, LocalDate expectedExpireDate){
         ExpireDateCalculator cal = new ExpireDateCalculator();
         LocalDate realExpireDate = cal.calculatorExpiryDate(billingDate, payAmount);
